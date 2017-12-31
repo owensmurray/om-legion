a8f44801cf570c1eb0ac748f54dad7b5c37ddd08
Rick Owens <rick@owensmurray.com>
2017-12-31 14:00:36 -0600

    no-op - whitespace

--- src/OM/Legion/Settings.hs
+++ src/OM/Legion/Settings.hs
@@ -11,15 +11,16 @@ import OM.Socket (Endpoint)
 {- | Settings used when starting up the legion framework runtime.  -}
 data RuntimeSettings = RuntimeSettings {
     peerBindAddr :: Endpoint,
-      {- ^
-        The address on which the legion framework will listen for
-        rebalancing and cluster management commands.
-      -}
+                    {- ^
+                      The address on which the legion framework will
+                      listen for rebalancing and cluster management
+                      commands.
+                    -}
     joinBindAddr :: Endpoint
-      {- ^
-        The address on which the legion framework will listen for cluster
-        join requests.
-      -}
+                    {- ^
+                      The address on which the legion framework will
+                      listen for cluster join requests.
+                    -}
   }
 
 
