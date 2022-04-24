cda3cb1554449ae05374e11577cc386f92ac2a04
Rick Owens <rick@owensmurray.com>
2022-04-24 11:19:24 -0500

    no-op: Documentation.

--- src/OM/Legion.hs
+++ src/OM/Legion.hs
@@ -1,33 +1,33 @@
 
 {- | Version 2 of the Legion framework. -}
 module OM.Legion (
-  -- * Starting up the runtime.
+  -- * ∙ Starting up the runtime.
   forkLegionary,
   EventConstraints,
   MonadConstraints,
   StartupMode(..),
   Runtime,
 
-  -- * Applying state changes.
+  -- * ∙ Applying state changes.
   applyFast,
   applyConsistent,
 
-  -- * Sending messages around the cluster.
+  -- * ∙ Sending messages around the cluster.
   cast,
   call,
   broadcast,
   broadcall,
 
-  -- * Inspecting the current state.
+  -- * ∙ Inspecting the current state.
   readState,
   getSelf,
   getClusterName,
 
-  -- * Observability
+  -- * ∙ Observability
   getStats,
   Stats(..),
 
-  -- * Cluster Topology
+  -- * ∙ Cluster Topology
   eject,
   Peer(..),
   ClusterName(..),
